5f63f273cefc9f0cd61a51ac60bf63e9a0ae55bb
Tom-cpu-maker <62932709+Tom-cpu-maker@users.noreply.github.com>
2020-03-31 11:13:17 +0200

    Test Tkinter Mini Projet 2 (Pierre Feuille Ciseaux)

---
+++ tkinter_pierre_feuille_papier_ciseaux.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from tkinter import *  
+Fenetre= Tk()  
+bouton1=Button(Fenetre, text="Papier") 
+bouton2=Button(Fenetre, text="Feuille")
+bouton3=Button(Fenetre, text="Ciseaux")  
+bouton1.pack() 
+bouton2.pack()
+bouton3.pack()
+Fenetre.mainloop()
+
